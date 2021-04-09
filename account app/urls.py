3cce13c725ed7839582a270894459b2b1b7ba259
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-09 21:56:14 +0300

    Update urls.py

--- account app/urls.py
+++ account app/urls.py
@@ -2,13 +2,17 @@
 # import of the re_path method for routing + import of view objects relative to requests from the views file
 from django.urls import re_path
 from . import views
+# импорт методов LoginView + LogoutView для представления шаблонов login.html + logout.html
+# import LoginView + LogoutView methods to view templates login.html + logout.html
+from django.contrib.auth.views import LoginView, LogoutView
 
-# определение названия приложения
-# define the name of the application
+
 app_name = 'account' 
 
 # шаблоны маршрутизаторов
 # router templates
 urlpatterns = [
-    re_path(r'^login/$', views.user_login, name='login')
+    re_path(r'^login/$', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    re_path(r'^logout/$', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    re_path(r'^$', views.profile, name='profile'),
 ]
