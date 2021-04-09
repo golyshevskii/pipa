76abbcfc687141811e7e26b9777d0fe1445f1b20
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-09 12:39:24 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,5 @@
-"""pipa URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# импорт объекта admin для административной части веб-сайта и методов re_path, include для маршрутизации
+# import of the admin object for the administrative part of the website and methods re_path, include for routing
 from django.contrib import admin
 from django.urls import re_path, include
 
