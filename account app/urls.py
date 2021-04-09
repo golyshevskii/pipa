7093f385fff4111b276a3c2600ed1ff011642788
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-09 12:02:38 +0300

    Update urls.py

--- account app/urls.py
+++ account app/urls.py
@@ -1,8 +1,14 @@
+# импорт метода re_path для маршрутизации + импорт объектов представления относительно запросов из файла views
+# import of the re_path method for routing + import of view objects relative to requests from the views file
 from django.urls import re_path
 from . import views
 
-app_name = 'account'
+# определение названия приложения
+# define the name of the application
+app_name = 'account' 
 
+# шаблоны маршрутизаторов
+# router templates
 urlpatterns = [
     re_path(r'^login/$', views.user_login, name='login')
 ]
