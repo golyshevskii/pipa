244534d831e878328f87a0c520fb944737618e74
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-09 11:56:23 +0300

    Update forms.py

--- account app/forms.py
+++ account app/forms.py
@@ -1,6 +1,12 @@
+# импорт объекта forms
+# import object forms
 from django import forms
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+# класс LoginForm для создания формы входа на веб-сайт + наследование объектов класса Form
+# class LoginForm to create a login form for the website + inheritance of objects of the Form class
+class LoginForm(forms.Form):        
+    # определение полей для логина и пароля
+    # defining fields for login and password
+    username = forms.CharField()             
+    password = forms.CharField(widget=forms.PasswordInput)  # ключ widget для скрытия ввода пароля пользователем; widget key to hide user password input
