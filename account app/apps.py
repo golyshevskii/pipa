a22ac2ee2c1c138d7d16512c8e852d5fdab32446
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-19 14:23:37 +0300

    Update apps.py

--- account app/apps.py
+++ account app/apps.py
@@ -1,13 +1,19 @@
 # импорт класса AppConfig для приложения account
+# importing the AppConfig class for the account application
 from django.apps import AppConfig
 # импорт класса Signal и метода send_activation_notification для отправки сообщения об активации пользователю
+# import of the Signal class and the send_activation_notification method to send an activation message to the user
 from django.dispatch import Signal
 from .utilities import send_activation_notification
 
 
+# экземпляр класса Signal для отправки сообщений об активации
+# an instance of the Signal class to send activation messages
 user_registered = Signal(providing_args=['instance'])
 
 
+# диспетчер отправки сообщений
+# dispatcher for sending messages
 def user_registered_dispatcher(sender, **kwargs):
     send_activation_notification(kwargs['instance'])
 
