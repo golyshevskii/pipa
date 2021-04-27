4f9872434dc5abcb47b9e68aff338d28984375d0
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-27 10:06:51 +0300

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # представление шаблона chatroom пользователю
+# presenting the chatroom template to the user
 @login_required
 def chat(request):
     return render(request, 'chat/chatroom.html')
