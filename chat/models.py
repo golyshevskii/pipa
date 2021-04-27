b58989ef2dfeb4e1597c3fba85b058ba84a86768
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-27 10:06:15 +0300

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 # определение базы данных сообщений
+# defining the message database
 class Message(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     text = models.TextField(null=True, blank=True)
